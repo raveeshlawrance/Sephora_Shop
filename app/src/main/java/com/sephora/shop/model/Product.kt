e4d1511bc16f54dfb5ab78ae70847be2bda31809
raveeshlawrance <raveesh.mca@gmail.com>
2022-02-13 12:18:25 +0800

    Initial commit - Base Setup

---
+++ app/src/main/java/com/sephora/shop/model/Product.kt
@@ -0,0 +1,3 @@
+package com.sephora.shop.model
+
+data class Product(val productName : String)
---
+++ app/src/main/java/com/sephora/shop/ui/productlist/ProductListActivity.kt
@@ -0,0 +1,12 @@
+package com.sephora.shop.ui.productlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sephora.shop.R
+
+class ProductListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_list)
+    }
+}
